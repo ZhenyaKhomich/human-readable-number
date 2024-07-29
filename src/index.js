0c68a39f2df6159d73c0458252ca43e00c9fa7ac
Zhenya Khomich <zhenyatsoy21061992@gmail.com>
2024-07-29 15:50:57 +0400

    finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function toReadable (number) {
-  
+  let n = number.toString().split('');
+
+  let num = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty'];
+
+  let num2 = ['zero', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+console.log(n.slice(1).join(''));
+  if(number <= 20) {
+    return num[number];
+  } else if( number < 100) {
+    if(n[n.length-1] == 0) {
+      return num2[n[0]];
+    }
+    return num2[n[0]] + " " + num[n[1]];
+  } else if (number < 1000) {
+    if(n[1] == 0 && n[2]==0) {
+      return num[n[0]] + ' ' + 'hundred';
+    } else if (n[1] == 0) {
+      return num[n[0]] + ' ' + 'hundred' + " " + num[n[2]];
+    } else if (+n.slice(1).join('') <= 20 ) {
+      return num[n[0]] + ' ' + 'hundred' + " " + num[n.slice(1).join('')]; 
+    } else if (n[2] == 0) {
+      return num[n[0]] + ' ' + 'hundred' + " " + num2[n[1]];
+    }
+      return num[n[0]] + ' ' + 'hundred' + " " + num2[n[1]] + " " + num[n[2]];
+    }
 }
